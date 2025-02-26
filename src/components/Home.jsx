2bbe1f0c1b988d19a5a2eefdcdbf04f0acb6b105
michabl116 <michabl@metropolia.fi>
2025-02-26 15:37:20 +0200

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,11 @@ function Home() {
     <div>
       <h2>Group 6</h2>
       <p>Code Marathon 1</p>
+      <ul>
+          <li>Farha</li>
+          <li>Fanaz</li>
+          <li>Michael</li>
+      </ul>
     </div>
   );
 }
